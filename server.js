82d5824b3e41b91325e15e96f52e3f4daa6508a0
Nagarjun A <nagarjun.avala.official@gmail.com>
2024-04-10 15:30:27 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+require("dotenv").config()
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(express.json());
+
+// Routes
+app.use("/api", require("./routes/authRouter"));
+
+const URI = process.env.MONGODB_URL || "mongodb://127.0.0.1:27017/agriculture_db";
+
+mongoose.connect(URI).then(() => {
+  console.log("Connected To MongoDB");
+}).catch((err) => {
+  throw new Error(err)
+});
+
+const port = process.env.PORT ||3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+app.get("/", (req, res) => {
+  res.cookie("cookie","cookie-secret")
+  res.send("Hello World!");
+});
